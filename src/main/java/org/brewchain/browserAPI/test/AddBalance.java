cfac2a6127a444ced3197619e3fa9a09de7fc884
fanyangyang <a985850124@qq.com>
2018-06-01 16:56:01 +0700

    add token balance and cryptotoken

--- src/main/java/org/brewchain/browserAPI/test/AddBalance.java
+++ src/main/java/org/brewchain/browserAPI/test/AddBalance.java
@@ -1,12 +1,17 @@
 package org.brewchain.browserAPI.test;
 
 
+import java.util.ArrayList;
+
 import org.apache.commons.lang3.StringUtils;
+import org.brewchain.account.gens.Act.AccountCryptoToken;
 import org.brewchain.browserAPI.gens.Lct.PLCTCommand;
 import org.brewchain.browserAPI.gens.Lct.PLCTModule;
 import org.brewchain.browserAPI.gens.Lct.ReqAdd;
 import org.brewchain.browserAPI.gens.Lct.RetAdd;
 import org.fc.brewchain.bcapi.EncAPI;
+
+import com.google.protobuf.ByteString;
 
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
@@ -41,14 +46,55 @@ public class AddBalance extends SessionModules<ReqAdd>{
 	@Override
 	public void onPBPacket(final FramePacket pack, final ReqAdd pb, final CompleteHandler handler) {
 		RetAdd.Builder ret = RetAdd.newBuilder();
-		
 		if(pb != null && StringUtils.isNotBlank(pb.getAddress())){
-			try {
-				oAccountHelper.addBalance(encApi.hexDec(pb.getAddress()), pb.getBalance());
-				org.brewchain.account.gens.Act.Account oAccount = oAccountHelper.GetAccount(encApi.hexDec(pb.getAddress()));
-				ret.setRetCode(1).setMsg("success").setBalance(oAccount.getValue().getBalance());
-			} catch (Exception e) {
-				ret.setRetCode(-1).setMsg("error : " + e.getMessage());
+			if(StringUtils.isNotBlank(pb.getToken())){
+				//erc20添加token
+				try {
+					oAccountHelper.addTokenBalance(encApi.hexDec(pb.getAddress()), pb.getToken(), pb.getBalance());
+					ret.setRetCode(1);
+				} catch (Exception e) {
+					ret.setRetCode(-1).setMsg(e.getMessage());
+				}
+			} else if (StringUtils.isNotBlank(pb.getSymbol())){
+				ArrayList<AccountCryptoToken.Builder> tokensList = new ArrayList<>();
+				long currentTime = System.currentTimeMillis();
+				for(int i = 1;i <= pb.getCount();i++){
+					try {
+						AccountCryptoToken.Builder oAccountCryptoToken = AccountCryptoToken.newBuilder();
+						oAccountCryptoToken.setCode(String.valueOf(i));
+						oAccountCryptoToken.setIndex( i - 1);
+						oAccountCryptoToken.setName("name" + i);
+						oAccountCryptoToken.setTimestamp(currentTime);
+						oAccountCryptoToken.setTotal(pb.getCount());
+						oAccountCryptoToken.setOwner(ByteString.copyFrom(encApi.hexDec(pb.getAddress())));
+						oAccountCryptoToken.setNonce(0);
+						oAccountCryptoToken.setHash(ByteString.copyFrom(encApi.sha256Encode(oAccountCryptoToken.build().toByteArray())));
+						tokensList.add(oAccountCryptoToken);
+					} catch (Exception e) {
+						System.out.println("第【"+i+"】条数据错误，错误原因："+e.getMessage());
+						log.debug("第【"+i+"】条数据错误，错误原因："+e.getMessage());
+						continue;
+					}
+				}
+				log.debug("拼装数据个数:"+tokensList.size());
+				long doneCount = 0;
+				try {
+					doneCount = oAccountHelper.newCryptoBalances(encApi.hexDec(pb.getAddress()), pb.getSymbol(), tokensList);
+				} catch (Exception e) {
+					ret.setRetCode(-1).setMsg("error : " + e.getMessage());
+				}
+				ret.setRetCode(1);
+				if(doneCount < pb.getCount()){
+					ret.setMsg("warn, new cryptoToken count is not equals to " + pb.getCount());
+				}
+			}else{
+				try {
+					oAccountHelper.addBalance(encApi.hexDec(pb.getAddress()), pb.getBalance());
+					org.brewchain.account.gens.Act.Account oAccount = oAccountHelper.GetAccount(encApi.hexDec(pb.getAddress()));
+					ret.setRetCode(1).setMsg("success").setBalance(oAccount.getValue().getBalance());
+				} catch (Exception e) {
+					ret.setRetCode(-1).setMsg("error : " + e.getMessage());
+				}
 			}
 		}else{
 			ret.setRetCode(-1).setMsg("no address");
